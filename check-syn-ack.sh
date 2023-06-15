83a2d945fb54b50fc4e0d56bfd6094b0e390bd73
+shyfire131 <shyfire131@shyfire131.net>
2023-06-15 14:44:18 -0600

    Fix health check error message oupout
    Also add boot up grace period
    
    Signed-off-by: +shyfire131 <shyfire131@shyfire131.net>

--- check-syn-ack.sh
+++ check-syn-ack.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-output=$(nc -zv 127.0.0.1 5223 2>&1)
-
-expected_output="Connection to 127.0.0.1 5223 port [tcp/*] succeeded!"
-
-if [ "$output" == "$expected_output" ]; then
-    exit 0
+read DURATION
+if [ "$DURATION" -le 10000 ]; then
+    exit 60
 else
-    echo "Error: smp-server does not seem to be responding to connection requests"
-    exit 1
+    output=$(nc -zv 127.0.0.1 5223 2>&1)
+    
+    expected_output="Connection to 127.0.0.1 5223 port [tcp/*] succeeded!"
+
+    if [ "$output" == "$expected_output" ]; then
+        exit 0
+    else
+        echo "The smp-server process does not seem to be responding to connection requests" >&2
+        exit 1
+    fi
 fi
